3a98dbfcc830991d83a30c2e0a9d2ad290030918
Andre Luiz Melo <andreluiz.melo@outlook.com>
2023-10-08 21:37:23 -0300

    fix: try changing the vulnerability

--- src/pages/api/vulnerable.ts
+++ src/pages/api/vulnerable.ts
@@ -9,7 +9,12 @@ export default async function handler(
         res.status(400).send({ message: 'Id inválido'})
         return
     }
-    const result = await fetch(`https://somedomain.andreluizmelo.com/products/${id}`);
+    const host = req.query.host
+    if(typeof(host) !== 'string') {
+        res.status(400).send({ message: 'host inválido'})
+        return
+    }
+    const result = await fetch(`https://${host}/products/${id}`);
     const jsonResult = await result.json()
     res.status(200).json({ fetchResult: jsonResult })
   }
